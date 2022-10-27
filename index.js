ca2bf67078ff19aca867ab37d5ad4985e9b691a9
unknown <cydturner01@gmail.com>
2022-10-27 16:12:47 -0400

    more js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-//let displayText = document.getElementById("display-text");
+let displayText = document.getElementById("display-text");
+let currentNum = 0;
+let expression = [];
 
 const numBttns = document.querySelectorAll('td[class=num-bttn]');
+const opBttns = document.querySelectorAll('td[class=op-bttn]');
 
 numBttns
 .forEach( bttn =>
 
     bttn.addEventListener("click", () => {
         console.log('clicked');
-        document.getElementById("display-text").textContent = bttn.textContent;
+        displayText.textContent = bttn.textContent;
     })
         
 );
 
+opBttns
+.forEach( bttn =>
+
+    bttn.addEventListener("click", () => {
+        console.log('operation');
+    })
+        
+);
